fb3bbc4ccd8498bcf693c7d0f358347d218e0e5b
Marc <mlanglois333@icloud.com>
2020-11-11 13:26:00 -0500

    connect to mongoose

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const routes = requre("./routes")
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,12 +14,13 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-
+app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+mongoose.connect(`mongodb+srv://Mlanglois:${process.env.DB_PASS}@cluster0.oy7eo.mongodb.net/googlebooks?retryWrites=true&w=majority`,(err)=>{
+  if(err) throw err;
+  console.log("DB Connected Successfully");
+  });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
