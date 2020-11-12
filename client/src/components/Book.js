9488d4e9cb74853fb4115112e8360e99dd325eac
Marc <mlanglois333@icloud.com>
2020-11-12 18:06:39 -0500

    Formatting

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -1,15 +1,28 @@
 import React from "react";
+import Container from "./Container";
+import Row from "./Row";
+import Col from "./Col";
+
 
 function Book (props) {
   return (
-    <div className="text-center">
-      <h3>Title</h3> <p>{props.title}</p>
-      <h3>Author </h3><p>{props.author}</p>
-      <h3>Description:</h3> <p>{props.description}</p>
-      <a href={props.link}>Click here to view on Google Books!</a>
-      <img src={props.image} alt="thumbbnail" />
-
-    </div>
+    <Container>
+      <Row>
+        <Col size="sm-6">
+        <img src={props.image} alt="thumbbnail" />
+        </Col>
+        <Col size="sm-6">
+      <h3>Author: </h3><p>{props.author}</p>
+      <h3>Description:</h3> <p>{props.description}</p>      
+      </Col>
+      </Row>
+      <Row>
+        <Col size="sm-12">
+        <a href={props.link}>Click here to view on Google Books!</a>
+        </Col>
+      </Row>
+  </Container>
+    
   );
 }
 
